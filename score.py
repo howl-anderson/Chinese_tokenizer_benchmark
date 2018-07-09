3316174b3db8e3892f64aad2812f156591a9a687
Xiaoquan Kong <u1mail2me@gmail.com>
2018-07-09 13:35:15 +0800

    parse_score is done!

--- score.py
+++ score.py
@@ -5,5 +5,22 @@
 def parse_score(score_file):
     with open(score_file, 'r') as fd:
         lines = fd.readlines()
-        last_line = lines[-1]
-        print(last_line)
+        raw_last_line = lines[-1]
+
+        last_line = raw_last_line.strip()
+
+        raw_metrics = last_line.split()
+
+        metrics = {
+            "RECALL": raw_metrics[8],
+            "PRECISION": raw_metrics[9],
+            "F1-MEASURE": raw_metrics[10]
+        }
+
+    return metrics
+
+
+if __name__ == "__main__":
+    score = parse_score("score_microtokenizer.ut8")
+    print(score)
+

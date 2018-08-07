5e2936d3434aa19247636530c5675c56428cf245
Xiaoquan Kong <u1mail2me@gmail.com>
2018-08-07 23:13:46 +0800

    Add MicroTokenizer train script

---
+++ train_MicroTokenizer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os
+
+from MicroTokenizer.train.train import train
+from main import corpus_registry, get_train_data_file
+
+for corpus in corpus_registry.values():
+    input_file_list = [get_train_data_file(corpus)]
+    output_dir = os.path.join("MicroTokenizer_model", corpus)
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    train(input_file_list, output_dir)

74fc4a5535578a6e169ba10ca8cb8cf0a9f08b57
Xiaoquan Kong <u1mail2me@gmail.com>
2018-08-31 17:54:53 +0800

    Add speed data and render code

--- render_readme.py
+++ render_readme.py
@@ -11,6 +11,12 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 input_file = os.path.join(current_dir, 'README.tpl.md')
 output_file = os.path.join(current_dir, 'README.md')
 
+
+def get_file_content(input_file):
+    with open(input_file) as fd:
+        return fd.read()
+
+
 with open(input_file, encoding='utf_8') as input_fd, open(output_file, mode='w', encoding='utf_8') as output_fd:
     template = Template(input_fd.read())
 
@@ -20,11 +26,14 @@ with open(input_file, encoding='utf_8') as input_fd, open(output_file, mode='w',
         markdown_table_file = os.path.join(current_dir, 'results', v + '.md')
         item = {
             'title': k,
-            'markdown_table': open(markdown_table_file).read()
+            'markdown_table': get_file_content(markdown_table_file)
         }
 
         test_result.append(item)
 
-    rendered_string = template.render(test_result=test_result)
+    speed_table_file = os.path.join(current_dir, 'speed.md')
+    speed_table = get_file_content(speed_table_file)
+
+    rendered_string = template.render(test_result=test_result, speed_table=speed_table)
 
     output_fd.write(rendered_string)
---
+++ speed.md
@@ -0,0 +1,10 @@
+| Algorithm                      |   Time Cost (seconds) |
+|:-------------------------------|----------------------:|
+| jieba                          |               4.68979 |
+| thulac                         |              29.0849  |
+| nlpir                          |               3.19577 |
+| ltp                            |               7.45371 |
+| MicroTokenizer_with_HMM        |              79.4509  |
+| MicroTokenizer_with_DAG        |              14.9891  |
+| MicroTokenizer_with_join_model |             103.775   |
+| MicroTokenizer_with_CRF        |               5.63067 |
